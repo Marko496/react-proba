db2f4b2c6a131bab55b6c016e1f9c117bbc94fd7
Marko496 <62379686+Marko496@users.noreply.github.com>
2020-05-11 11:52:21 +0200

    Updates

---
+++ precache-manifest.8ea1c59fb0ea6acfe4ced361a97b5fed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf21e8d0970000d96dae56b3dac5d05",
+    "url": "/react-proba/index.html"
+  },
+  {
+    "revision": "3504c627dd5877b16f3a",
+    "url": "/react-proba/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6856eda0bd10aa3dcd72",
+    "url": "/react-proba/static/js/2.421bf6c4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-proba/static/js/2.421bf6c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3504c627dd5877b16f3a",
+    "url": "/react-proba/static/js/main.ef92760f.chunk.js"
+  },
+  {
+    "revision": "42665a12fcbb270ac8d6",
+    "url": "/react-proba/static/js/runtime-main.0b937ab4.js"
+  },
+  {
+    "revision": "d4eebba4f5b52b7deb45ad268d83568c",
+    "url": "/react-proba/static/media/nut.d4eebba4.jpg"
+  }
+]);
